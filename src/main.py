42d5ce062a65281634474fdda086469a5ffc06d8
MR POOL <vishnumrpool@gmail.com>
2026-01-14 12:50:28 +0530

    main looping concept

--- src/main.py
+++ src/main.py
@@ -1,30 +1,39 @@
+import msvcrt
+
 from mail_builder import create_template, find_mailID
 from outlook import generate_draft
 from logger import logger
 
 def main():
-    team_name = input("Enter the team name: ").strip()
-    incident_id = input("Enter the incident id: ").strip()
-    #user_name = input("Enter the sender name: ").strip()
+    while True: 
+        team_name = input("Enter the team name: ").strip()
+        incident_id = input("Enter the incident id: ").strip()
+        #user_name = input("Enter the sender name: ").strip()
 
-    #create to and cc emails
-    try:
-        to, cc = find_mailID(team_name)
-        cc_list = list()
-        cc_list.extend([cc,"pratap.chauhan@dxc.com", "poonam.rane@dxc.com", "Security.admin@bankofbarod.co.in","security.operator@bankofbaroda.co.in"])
-    except Exception as e: 
-        logger.exception(f"Failed to find team and CC: {e}")
-        return False
+        #create to and cc emails
+        try:
+            to, cc = find_mailID(team_name)
+            cc_list = list()
+            cc_list.extend([cc,"pratap.chauhan@dxc.com", "poonam.rane@dxc.com", "Security.admin@bankofbarod.co.in","security.operator@bankofbaroda.co.in"])
+        except Exception as e: 
+            logger.exception(f"Failed to find team and CC: {e}")
+            return False
 
-    #create template
-    subject, template = create_template(team_name,incident_id)
+        #create template
+        subject, template = create_template(team_name,incident_id)
 
-    #generate outlook draft
-    try:
-        generate_draft(to, cc_list, subject, template)
-    except Exception as e:
-        logger.exception(f"Failed to draft mail: {e}")
-        return False
+        #generate outlook draft
+        try:
+            generate_draft(to, cc_list, subject, template)
+        except Exception as e:
+            logger.exception(f"Failed to draft mail: {e}")
+            return False
+
+        #looping logic
+        if msvcrt.kbhit() and msvcrt.getch() == b'\x1b':
+            print("Esc pressed, exiting...")
+            exit()
+
 
 if __name__ == "__main__":
     try:
